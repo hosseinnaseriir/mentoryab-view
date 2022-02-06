816a3f2c2059edabc0e1018578d596da94643369
Hossein Naseri <hossein19naseri99@gmail.com>
2022-02-07 02:08:38 +0330

    upload file

--- screens/auth/CompleteRegister/index.js
+++ screens/auth/CompleteRegister/index.js
@@ -1,35 +1,29 @@
-import React, {
-  useRef
-} from "react";
+import React, { useRef } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Typography from "../../../components/common/Typography";
 import TextField from "../../../components/common/TextField";
-import {
-  useState
-} from "react";
-import {
-  svgIcons
-} from "../../../assets/icons/svgIcons";
+import { useState } from "react";
+import { svgIcons } from "../../../assets/icons/svgIcons";
 import SimpleReactValidator from "simple-react-validator";
 
-import Button from '../../../components/common/Button';
+import Button from "../../../components/common/Button";
 import JobInformation from "./jobInformation";
 import LocationInformation from "./LocationInformation";
 import ContactInformation from "./ContactInformation";
-import AddAvatar from './AddAvatar';
-import {
-  Box
-} from '@mui/material';
-import postFetch from './../../../utils/postFetch';
-import {
-  BASE_API
-} from './../../../api/index';
-import { Autocomplete } from '@mui/material';
-import { getCookies } from 'cookies-next';
-import { useSelector } from 'react-redux';
+import AddAvatar from "./AddAvatar";
+import { Box } from "@mui/material";
+import postFetch from "./../../../utils/postFetch";
+import { BASE_API } from "./../../../api/index";
+import { Autocomplete } from "@mui/material";
+import { getCookies } from "cookies-next";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 const CompleteRegister = () => {
+  const router = useRouter();
+
   const simpleValidator = useRef(
     new SimpleReactValidator({
       className: "text-danger",
@@ -58,9 +52,9 @@ const CompleteRegister = () => {
   const [phoneNumber, setPhoneNumber] = useState();
   const [website, setWebsite] = useState();
 
-  const { userDetails} = useSelector(state =>  state.userSlice);
+  const { userDetails } = useSelector((state) => state.userSlice);
 
-  console.log(userDetails)
+  console.log(userDetails);
 
   const handleCompleteProfile = (e) => {
     e.preventDefault();
@@ -68,35 +62,42 @@ const CompleteRegister = () => {
 
     const formData = new FormData();
 
-    formData.append('userID', userDetails.userID);  
-    formData.append('specialty', specialty);  
-    formData.append('avatar', avatar);
-    formData.append('resume', resumeFile?.resume);
-    formData.append('personPosition', personPosition);
-    formData.append('company', company);
-    formData.append('workExperience', workExperience);
-    formData.append('province', province);
-    formData.append('city', city);
-    formData.append('address', address);
-    formData.append('birthDay', birthDay);
-    formData.append('linkedin', linkedin);
-    formData.append('instagram', instagram);
-    formData.append('phoneNumber', phoneNumber);
-    formData.append('website', website);
- 
+    formData.append("userID", userDetails.userID || '');
+    formData.append("specialty", specialty || '');
+    formData.append("avatar", avatar || '');
+    formData.append("resume", resumeFile?.resume || '');
+    formData.append("personPosition", personPosition || '');
+    formData.append("company", company || '');
+    formData.append("workExperience", workExperience || '');
+    formData.append("province", province || '');
+    formData.append("city", city || '');
+
+    formData.append("address", address || '');
+    formData.append("birthDay", birthDay || '');
+    formData.append("socialMedia", JSON.stringify([{ linkedin }, { instagram }, { website }]) || '');
+    // formData.append("linkedin", linkedin || '');
+    // formData.append("instagram", instagram || '');
+    // formData.append("website", website || '');
+    formData.append("phoneNumber", phoneNumber || '');
+
     for (var pair of formData.entries()) {
-        console.log(pair[0]+ ', ' + pair[1]); 
+      console.log(pair[0] + ", " + pair[1]);
     }
-    
+
     postFetch(true, `${BASE_API}/auth/complete-register`, formData, {
       "ath-token": getCookies("token").token,
-      "Content-Type": "multipart/form-data"
-    }).then(res => {
-      console.log(res)
+      "Content-Type": "multipart/form-data",
     })
-  }
-  console.log('specialty', specialty)
- 
+      .then((res) => {
+        console.log(res);
+        router.push("/");
+        toast.success(res?.data?.message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  console.log("specialty", specialty);
 
   return (
     <>
@@ -106,10 +107,9 @@ const CompleteRegister = () => {
       </Head>
       <main className="container">
         <form>
-         <AddAvatar setAvatar={setAvatar}/>
+          <AddAvatar setAvatar={setAvatar} />
           <Box className="row mt-md-5 pt-md-5 mb-5">
- 
-           <JobInformation
+            <JobInformation
               specialty={specialty}
               setSpecialty={setSpecialty}
               personPosition={personPosition}
@@ -122,19 +122,19 @@ const CompleteRegister = () => {
               setResume={setResume}
               setResumeFile={setResumeFile}
               simpleValidator={simpleValidator}
-           />
-           <LocationInformation
-               province={province}
-               setProvince={setProvince}
-               simpleValidator={simpleValidator}
-               city={city}
-               setCity={setCity}
-               address={address}
-               setAddress={setAddress}
-               birthDay={birthDay}
-               setBirthDay={setBirthDay}
-           />
-          <ContactInformation
+            />
+            <LocationInformation
+              province={province}
+              setProvince={setProvince}
+              simpleValidator={simpleValidator}
+              city={city}
+              setCity={setCity}
+              address={address}
+              setAddress={setAddress}
+              birthDay={birthDay}
+              setBirthDay={setBirthDay}
+            />
+            <ContactInformation
               simpleValidator={simpleValidator}
               linkedin={linkedin}
               setLinkedin={setLinkedin}
@@ -144,9 +144,15 @@ const CompleteRegister = () => {
               setPhoneNumber={setPhoneNumber}
               website={website}
               setWebsite={setWebsite}
-          />
+            />
             {/* <Button variant='secondary' type='submit' onClick={e=>handleCompleteProfile(e)} parentClassName='d-flex flex-row-reverse' >باشه برای بعد</Button> */}
-            <Button type='submit' onClick={e=>handleCompleteProfile(e)} parentClassName='d-flex flex-row-reverse' >ثبت و ورود</Button>
+            <Button
+              type="submit"
+              onClick={(e) => handleCompleteProfile(e)}
+              parentClassName="d-flex flex-row-reverse"
+            >
+              ثبت و ورود
+            </Button>
           </Box>
         </form>
       </main>
